926769bf40430fe879e728a77cf06cdce6b860eb
Egor Panfilov <egor.v.panfilov@gmail.com>
2019-09-04 19:59:37 +0300

    Finalized 2D gray affine test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,87 +1,43 @@
 import pytest
 import imageio
-from skimage import transform, color, util
-import matplotlib.pyplot as plt
+from skimage import transform, color
 
 import pyelastix
 
 
-# @pytest.fixture
-# def image_fixed():
-#     pass
-#
-#
-# @pytest.fixture
-# def image_moving():
-#     pass
+# TODO: add test to check how elastix detection works
+# TODO: add test with 3D images
+# TODO: add test with series
+# TODO: add test with bspline transform
 
 
 def test_register_affine_gray():
+    # Get fixed image
     image_fixed = imageio.imread('imageio:chelsea.png')
     image_fixed = color.rgb2gray(image_fixed)
 
+    # Generate moving image
     image_moving = transform.rotate(image_fixed,
                                     angle=15,
                                     resize=True)
 
-    image_fixed = util.img_as_ubyte(image_fixed)
-    image_moving = util.img_as_ubyte(image_moving)
-
-    # image_moving = transform.rescale(image_moving,
-    #                                  scale=1.5,
-    #                                  mode='constant',
-    #                                  multichannel=False)
-
-    # Select one channel (grayscale), and make float
+    # Convert both images to float32
     image_fixed = image_fixed.astype('float32')
     image_moving = image_moving.astype('float32')
 
-    # Get default params and adjust
+    # Initialize and adjust the parameters
     params = pyelastix.get_default_params(type='AFFINE')
 
     params.FixedInternalImagePixelType = "float"
     params.MovingInternalImagePixelType = "float"
     params.ResultImagePixelType = "float"
 
-    # params.Transform = "AffineTransform"
-    # params.HowToCombineTransforms = "Compose"
-    # params.AutomaticTransformInitialization = "true"
-    # params.AutomaticScalesEstimation = "true"
-
-    # params.Registration = "MultiResolutionRegistration"
-    # params.FixedImagePyramid = "FixedRecursiveImagePyramid"
-    # params.MovingImagePyramid = "MovingRecursiveImagePyramid"
-
-    params.NumberOfResolutions = 2
-    params.MaximumNumberOfIterations = 500
-
-    # params.ImageSampler = "RandomCoordinate"
-    # params.FixedImageBSplineInterpolationOrder = 1
-    # params.UseRandomSampleRegion = "false"
-    # params.NumberOfSpatialSamples = 1024
-    # params.NewSamplesEveryIteration = "true"
-    # params.CheckNumberOfSamples = "true"
-    # params.MaximumNumberOfSamplingAttempts = 10
-    #
-    # params.Metric = "AdvancedNormalizedCorrelation"
-
-    print(params)
+    params.NumberOfResolutions = 3
+    params.MaximumNumberOfIterations = 1000
 
     # Register
     image_registered, field = pyelastix.register(
         image_moving, image_fixed, params)
-        # image_fixed, image_moving, params)
 
-    # TODO: finalize
-    assert 1 == 1
-
-    # Visualize the result
-    # fig, axes = plt.subplots(2, 3)
-    # axes = axes.ravel()
-    # axes[0].imshow(image_fixed)
-    # axes[1].imshow(image_moving)
-    # axes[2].imshow(image_registered)
-    # axes[3].imshow(field[0])
-    # axes[4].imshow(field[1])
-    #
-    # plt.show()
+    # Check the results
+    assert image_registered == pytest.approx(image_fixed, rel=1)
